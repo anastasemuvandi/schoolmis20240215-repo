8726224fc1008ad86734c12f4d304e6a3c06697c
TuriImfura <alexanderturiimfura@gmail.com>
2024-02-15 11:21:56 +0200

    We have added bootstrap to index

--- src/index.php
+++ src/index.php
@@ -1,1 +1,12 @@
-<?php echo "Welcome to the web"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>School MIS</title>
+</head>
+<body>
+    <?php echo "<h3>Welcome to the web</h3>"; ?>
+</body>
+</html>
